3abaf4d7ed7012e15874e792d712b429b120e02f
Ritty <ritty@users.noreply.github.com>
2026-08-03 10:15:46 +0000

    fix: SW 注册绕过 HTTP 缓存 + 自动刷新更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'ritty-v7';
+const VERSION = 'ritty-v8';
 const STATIC = ['.', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
